69c31d39d27157f1c020909a4ac11186df8ba7ff
beanstown106 <nbenis106@gmail.com>
2016-10-17 02:05:25 -0400

    Custom Carrier Label & Carrier Label Placement [2/2]
    Credit/Thanks to @Altaf-Mahdi @beanstown106 and @martincz
    
    Change-Id: Ia7b29f85d7a1286bd15dc4427727a683e133ab3f
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/org/candy/candyshop/tabs/StatusBar.java
+++ src/org/candy/candyshop/tabs/StatusBar.java
@@ -16,8 +16,13 @@
 
 package org.candy.candyshop.tabs;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.ContentResolver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnCancelListener;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
@@ -31,9 +36,13 @@ import android.support.v7.preference.PreferenceScreen;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 import android.support.v14.preference.PreferenceFragment;
 import android.support.v14.preference.SwitchPreference;
+import android.telephony.TelephonyManager;
+import android.text.Spannable;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.android.internal.logging.nano.MetricsProto;
 import com.android.settings.Utils;
@@ -58,6 +67,10 @@ public class StatusBar extends SettingsPreferenceFragment implements
     private static final String SHOW_LTE_FOURGEE = "show_lte_fourgee";
     private SwitchPreference mShowLteFourGee;
 
+    private static final String CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+    private PreferenceScreen mCustomCarrierLabel;
+    private String mCustomCarrierLabelText;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -69,6 +82,9 @@ public class StatusBar extends SettingsPreferenceFragment implements
                 Settings.System.SHOW_LTE_FOURGEE, 0) == 1));
         mShowLteFourGee.setOnPreferenceChangeListener(this);
 
+        // custom carrier label
+        mCustomCarrierLabel = (PreferenceScreen) findPreference(CUSTOM_CARRIER_LABEL);
+        updateCustomLabelTextSummary();
     }
 
     @Override
@@ -94,6 +110,46 @@ public class StatusBar extends SettingsPreferenceFragment implements
             return true;
         }
         return false;
+    }
+
+    public boolean onPreferenceTreeClick(Preference preference) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        boolean value;
+        if (preference == mCustomCarrierLabel) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+            alert.setTitle(R.string.custom_carrier_label_title);
+            alert.setMessage(R.string.custom_carrier_label_explain);
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getActivity());
+            input.setText(TextUtils.isEmpty(mCustomCarrierLabelText) ? "" : mCustomCarrierLabelText);
+            input.setSelection(input.getText().length());
+            alert.setView(input);
+            alert.setPositiveButton(getString(android.R.string.ok),
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int whichButton) {
+                            String value = ((Spannable) input.getText()).toString().trim();
+                            Settings.System.putString(resolver, Settings.System.CUSTOM_CARRIER_LABEL, value);
+                            updateCustomLabelTextSummary();
+                            Intent i = new Intent();
+                            i.setAction(Intent.ACTION_CUSTOM_CARRIER_LABEL_CHANGED);
+                            getActivity().sendBroadcast(i);
+                }
+            });
+            alert.setNegativeButton(getString(android.R.string.cancel), null);
+            alert.show();
+            return true;
+        }
+        return false;
+    }
+
+    private void updateCustomLabelTextSummary() {
+        mCustomCarrierLabelText = Settings.System.getString(
+            getContentResolver(), Settings.System.CUSTOM_CARRIER_LABEL);
+        if (TextUtils.isEmpty(mCustomCarrierLabelText)) {
+            mCustomCarrierLabel.setSummary(R.string.custom_carrier_label_notset);
+        } else {
+            mCustomCarrierLabel.setSummary(mCustomCarrierLabelText);
+        }
     }
 
     public static final Indexable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
